57ab6abc0b22a836e3109071e40f8cf19b0ec628
ShaluSukumaran <shalu@ca.ibm.com>
2017-05-30 14:00:51 -0400

    Created a program to display my name

---
+++ Week1/HelloWorldClass.java
@@ -0,0 +1,14 @@
+// single comment
+/*Multi line comment */
+
+
+/*This is a simple java program. Call this file  "HelloWorldClass.java:>*/
+
+
+public class HelloWorldClass
+{
+	public static void main (String args[])
+	{
+		System.out.println("Hello World123");
+	}
+}
